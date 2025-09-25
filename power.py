151e9dab7a7ed4e2dcd681b6de20dc3810c2c92a
Sanjay Kliche <s_kliche@gmx.de>
2025-09-25 12:44:32 +0200

    VariablenHello

--- power.py
+++ power.py
@@ -1,1 +1,11 @@
-print("Hello!")
+name="Sanjay"
+age="28"
+country="Deutschland"
+
+result="Hallo, mein Name ist "+name+", ich bin "+age+" Jahre alt und komme aus "+country #in Variable geschrieben
+print(result)
+
+result2=f"Hallo, mein Name ist {name}, ich bin {age} Jahre alt und komme aus {country}" #in string Variable geschrieben
+print(result2)
+
+print("Hallo, mein Name ist "+name+", ich bin "+age+" Jahre alt und komme aus "+country) #direkt geprintet
